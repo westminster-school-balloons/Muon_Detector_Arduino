7ad79a7e7fe4fa056ed071671e913f79bda0c67c
Kazuma8 <kazumasa3miura@gmail.com>
2024-11-13 09:54:40 +0000

    Reorder functions for Arduino Compiler

--- Arduino/Flight_Computer/Flight_Computer.ino
+++ Arduino/Flight_Computer/Flight_Computer.ino
@@ -40,7 +40,6 @@ File myFile;
 #include <TimerOne.h>
 #include <Wire.h>
 #include <SPI.h>
-#include <EEPROM.h>
 
 const byte OLED = 1;                      // Turn on/off the OLED [1,0]
 
@@ -92,80 +91,127 @@ int           Mode                          = 1;          // Different functions
 byte waiting_for_interupt                     = 0;
 byte SLAVE;
 byte MASTER;
-byte keep_pulse;
+byte keep_pulse                               = 0;
 
-
-void setup() {
-    analogReference (EXTERNAL);
-    ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2));      // clear prescaler bits
-    //ADCSRA |= bit (ADPS1);                                   // Set prescaler to 4  
-    ADCSRA |= bit (ADPS0) | bit (ADPS1); // Set prescaler to 8
-
-    //Parallel Detector Capability
-    get_detector_name(detector_name);
-    pinMode(3, OUTPUT); 
-    pinMode(6, INPUT);
-
-    Serial.begin(9600);
-    Serial.setTimeout(3000);
-
-    if (digitalRead(6) == HIGH){
-        filename[4] = 'S';
-        SLAVE = 1;
-        MASTER = 0;
+float get_sipm_voltage(float adc_value)
+{
+    float voltage = 0;
+    for (int i = 0; i < (sizeof(cal)/sizeof(float)); i++) {
+        voltage += cal[i] * pow(adc_value,(sizeof(cal)/sizeof(float)-i-1));
     }
-
-    else{
-        //delay(10);
-        filename[4] = 'M';
-        MASTER = 1;
-        SLAVE = 0;
-        pinMode(6, OUTPUT);
-        digitalWrite(6,HIGH);
-        //delay(2000);
-    }
-
-    //If OLED is active
-    if (OLED == 1){
-        display.setRotation(2);         // Upside down screen (0 is right-side-up)
-        OpeningScreen();                // Run the splash screen on start-up
-        delay(2000);                    // Delay some time to show the logo, and keep the Pin6 HIGH for coincidence
-        display.setTextSize(1);
-    }
-
-    if (!SD.begin(SDPIN)) {
-    Serial.println(F("SD initialisation failed"));
-    //Serial.println(F("Is there an SD card inserted?"));
-    }
-    else{
-        Serial.println(F("SD initialisation succeeded"))
-    }
-  
-/*      //Queries whether to write to SD Card
-    get_Mode();
-    if (Mode == 2) read_from_SD();
-    else if (Mode == 3) remove_all_SD();
-    else{setup_files();}
-*/
-
-    digitalWrite(3,LOW);
-    if (MASTER == 1){digitalWrite(6,LOW);}
-    analogRead(A0);
-
-    get_time();
-    start_time = millis();
-
-    if(Mode == 1){
-        myFile.println(F("##########################################################################################"));
-        myFile.println(F("### CosmicWatch: The Desktop Muon Detector"));
-        myFile.println(F("### Comp_date Comp_time Event Ardn_time[ms] ADC[0-1023] SiPM[mV] Deadtime[ms] Temp[C] Name"));
-        myFile.println(F("##########################################################################################"));
-        myFile.println("Device ID: " + (String)detector_name);
-    }
-
-    Timer1.initialize(TIMER_INTERVAL);             // Initialise timer 1
-    Timer1.attachInterrupt(timerIsr);              // attach the ISR routine
+    return voltage;
 }
+
+boolean get_detector_name(char* det_name) 
+{
+    byte ch;                              // byte read from eeprom
+    int bytesRead = 0;                    // number of bytes read so far
+    ch = EEPROM.read(bytesRead);          // read next byte from eeprom
+    det_name[bytesRead] = ch;               // store it into the user buffer
+    bytesRead++;                          // increment byte counter
+
+    while ( (ch != 0x00) && (bytesRead < 40) && ((bytesRead) <= 511) ) 
+    {
+        ch = EEPROM.read(bytesRead);
+        det_name[bytesRead] = ch;           // store it into the user buffer
+        bytesRead++;                      // increment byte counter
+    }
+    if ((ch != 0x00) && (bytesRead >= 1)) {det_name[bytesRead - 1] = 0;}
+    return true;
+}
+
+//OLED screen functions
+
+void get_time() 
+{
+    unsigned long int OLED_t1             = micros();
+    float count_average                   = 0;
+    float count_std                       = 0;
+
+    if (count > 0.) {
+        count_average   = count / ((interrupt_timer - start_time - total_deadtime) / 1000.);
+        count_std       = sqrt(count) / ((interrupt_timer - start_time - total_deadtime) / 1000.);}
+    else {
+        count_average   = 0;
+        count_std       = 0;
+    }
+    
+    display.setCursor(0, 0);
+    display.clearDisplay();
+    display.print(F("Total Count: "));
+    display.println(count);
+    display.print(F("Uptime: "));
+
+    int minutes                 = ((interrupt_timer - start_time) / 1000 / 60) % 60;
+    int seconds                 = ((interrupt_timer - start_time) / 1000) % 60;
+    char min_char[4];
+    char sec_char[4];
+    
+    sprintf(min_char, "%02d", minutes);
+    sprintf(sec_char, "%02d", seconds);
+
+    display.println((String) ((interrupt_timer - start_time) / 1000 / 3600) + ":" + min_char + ":" + sec_char);
+
+    if (count == 0) {
+        display.println("Hi, I'm "+(String)detector_name);
+        }
+        //if (MASTER == 1) {display.println(F("::---  MASTER   ---::"));}
+        //if (SLAVE  == 1) {display.println(F("::---   SLAVE   ---::"));}}
+        
+    else{
+        if (last_sipm_voltage > 180){
+            display.print(F("===---- WOW! ----==="));}
+        else{
+                if (MASTER == 1) {display.print(F("M"));}
+                if (SLAVE  == 1) {display.print(F("S"));}
+                for (int i = 1; i <=  (last_sipm_voltage + 10) / 10; i++) {display.print(F("-"));}}
+        display.println(F(""));
+    }
+
+    char tmp_average[4];
+    char tmp_std[4];
+
+    int decimals = 2;
+    if (count_average < 10) {decimals = 3;}
+    
+    dtostrf(count_average, 1, decimals, tmp_average);
+    dtostrf(count_std, 1, decimals, tmp_std);
+    
+    display.print(F("Rate: "));
+    display.print((String)tmp_average);
+    display.print(F("+/-"));
+    display.println((String)tmp_std);
+    display.display();
+    
+    total_deadtime                      += (micros() - OLED_t1 +73)/1000.;
+}
+
+
+void timerIsr() 
+{
+    interrupts();
+    interrupt_timer                       = millis();
+    if (waiting_for_interupt == 1){
+        total_deadtime += (millis() - waiting_t1);}
+    waiting_for_interupt = 0;
+    if (OLED == 1){
+        get_time();}
+}
+
+
+void OpeningScreen(void) 
+{
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(8, 0);
+    display.clearDisplay();
+    display.print(F("Cosmic \n     Watch"));
+    display.display();
+    display.setTextSize(1);
+    display.clearDisplay();
+}
+
+
 
 void transfer_to_SD_and_Pi(){ 
   while (1){
@@ -240,132 +286,6 @@ void transfer_to_SD_and_Pi(){
         total_deadtime += (micros() - measurement_t1) / 1000.;}
     }
 }
-
-
-float get_sipm_voltage(float adc_value
-{
-    float voltage = 0;
-    for (int i = 0; i < (sizeof(cal)/sizeof(float)); i++) {
-        voltage += cal[i] * pow(adc_value,(sizeof(cal)/sizeof(float)-i-1));
-    }
-    return voltage;
-}
-
-boolean get_detector_name(char* det_name) 
-{
-    byte ch;                              // byte read from eeprom
-    int bytesRead = 0;                    // number of bytes read so far
-    ch = EEPROM.read(bytesRead);          // read next byte from eeprom
-    det_name[bytesRead] = ch;               // store it into the user buffer
-    bytesRead++;                          // increment byte counter
-
-    while ( (ch != 0x00) && (bytesRead < 40) && ((bytesRead) <= 511) ) 
-    {
-        ch = EEPROM.read(bytesRead);
-        det_name[bytesRead] = ch;           // store it into the user buffer
-        bytesRead++;                      // increment byte counter
-    }
-    if ((ch != 0x00) && (bytesRead >= 1)) {det_name[bytesRead - 1] = 0;}
-    return true;
-}
-
-
-void loop() {
-    transfer_to_SD_and_Pi();
-    }
-}
-
-//OLED screen functions
-
-void timerIsr() 
-{
-    interrupts();
-    interrupt_timer                       = millis();
-    if (waiting_for_interupt == 1){
-        total_deadtime += (millis() - waiting_t1);}
-    waiting_for_interupt = 0;
-    if (OLED == 1){
-        get_time();}
-}
-
-void get_time() 
-{
-    unsigned long int OLED_t1             = micros();
-    float count_average                   = 0;
-    float count_std                       = 0;
-
-    if (count > 0.) {
-        count_average   = count / ((interrupt_timer - start_time - total_deadtime) / 1000.);
-        count_std       = sqrt(count) / ((interrupt_timer - start_time - total_deadtime) / 1000.);}
-    else {
-        count_average   = 0;
-        count_std       = 0;
-    }
-    
-    display.setCursor(0, 0);
-    display.clearDisplay();
-    display.print(F("Total Count: "));
-    display.println(count);
-    display.print(F("Uptime: "));
-
-    int minutes                 = ((interrupt_timer - start_time) / 1000 / 60) % 60;
-    int seconds                 = ((interrupt_timer - start_time) / 1000) % 60;
-    char min_char[4];
-    char sec_char[4];
-    
-    sprintf(min_char, "%02d", minutes);
-    sprintf(sec_char, "%02d", seconds);
-
-    display.println((String) ((interrupt_timer - start_time) / 1000 / 3600) + ":" + min_char + ":" + sec_char);
-
-    if (count == 0) {
-        display.println("Hi, I'm "+(String)detector_name);
-        }
-        //if (MASTER == 1) {display.println(F("::---  MASTER   ---::"));}
-        //if (SLAVE  == 1) {display.println(F("::---   SLAVE   ---::"));}}
-        
-    else{
-        if (last_sipm_voltage > 180){
-            display.print(F("===---- WOW! ----==="));}
-        else{
-                if (MASTER == 1) {display.print(F("M"));}
-                if (SLAVE  == 1) {display.print(F("S"));}
-                for (int i = 1; i <=  (last_sipm_voltage + 10) / 10; i++) {display.print(F("-"));}}
-        display.println(F(""));
-    }
-
-    char tmp_average[4];
-    char tmp_std[4];
-
-    int decimals = 2;
-    if (count_average < 10) {decimals = 3;}
-    
-    dtostrf(count_average, 1, decimals, tmp_average);
-    dtostrf(count_std, 1, decimals, tmp_std);
-    
-    display.print(F("Rate: "));
-    display.print((String)tmp_average);
-    display.print(F("+/-"));
-    display.println((String)tmp_std);
-    display.display();
-    
-    total_deadtime                      += (micros() - OLED_t1 +73)/1000.;
-}
-
-void OpeningScreen(void) 
-{
-    display.setTextSize(2);
-    display.setTextColor(WHITE);
-    display.setCursor(8, 0);
-    display.clearDisplay();
-    display.print(F("Cosmic \n     Watch"));
-    display.display();
-    display.setTextSize(1);
-    display.clearDisplay();
-}
-
-
-
 
 /*
 
@@ -497,3 +417,81 @@ void get_Mode(){ //fuction for automatic port finding on PC
     }
 }
 */
+
+void setup() {
+    analogReference (EXTERNAL);
+    ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2));      // clear prescaler bits
+    //ADCSRA |= bit (ADPS1);                                   // Set prescaler to 4  
+    ADCSRA |= bit (ADPS0) | bit (ADPS1); // Set prescaler to 8
+
+    //Parallel Detector Capability
+    get_detector_name(detector_name);
+    pinMode(3, OUTPUT); 
+    pinMode(6, INPUT);
+
+    Serial.begin(9600);
+    Serial.setTimeout(3000);
+
+    if (digitalRead(6) == HIGH){
+        filename[4] = 'S';
+        SLAVE = 1;
+        MASTER = 0;
+    }
+
+    else{
+        //delay(10);
+        filename[4] = 'M';
+        MASTER = 1;
+        SLAVE = 0;
+        pinMode(6, OUTPUT);
+        digitalWrite(6,HIGH);
+        //delay(2000);
+    }
+
+    //If OLED is active
+    if (OLED == 1){
+        display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+        display.setRotation(2);         // Upside down screen (0 is right-side-up)
+        OpeningScreen();                // Run the splash screen on start-up
+        delay(2000);                    // Delay some time to show the logo, and keep the Pin6 HIGH for coincidence
+        display.setTextSize(1);
+    }
+
+    if (!SD.begin(SDPIN)) {
+    Serial.println(F("SD initialisation failed"));
+    //Serial.println(F("Is there an SD card inserted?"));
+    }
+    else{
+        Serial.println(F("SD initialisation succeeded"));
+    }
+  
+/*      //Queries whether to write to SD Card
+    get_Mode();
+    if (Mode == 2) read_from_SD();
+    else if (Mode == 3) remove_all_SD();
+    else{setup_files();}
+*/
+
+    digitalWrite(3,LOW);
+    if (MASTER == 1){digitalWrite(6,LOW);}
+    analogRead(A0);
+
+    get_time();
+    start_time = millis();
+
+    if(Mode == 1){
+        myFile.println(F("##########################################################################################"));
+        myFile.println(F("### CosmicWatch: The Desktop Muon Detector"));
+        myFile.println(F("### Comp_date Comp_time Event Ardn_time[ms] ADC[0-1023] SiPM[mV] Deadtime[ms] Temp[C] Name"));
+        myFile.println(F("##########################################################################################"));
+        myFile.println("Device ID: " + (String)detector_name);
+    }
+
+    Timer1.initialize(TIMER_INTERVAL);             // Initialise timer 1
+    Timer1.attachInterrupt(timerIsr);              // attach the ISR routine
+}
+
+void loop() {
+  transfer_to_SD_and_Pi();
+}
+
